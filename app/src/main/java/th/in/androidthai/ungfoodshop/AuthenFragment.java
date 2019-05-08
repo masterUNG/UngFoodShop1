cec0afd4d645402819d18bf5d2fc18314c804052
masterUNG <phrombutr@gmail.com>
2019-05-08 15:37:44 +0700

    Read JSON Success

--- app/src/main/java/th/in/androidthai/ungfoodshop/AuthenFragment.java
+++ app/src/main/java/th/in/androidthai/ungfoodshop/AuthenFragment.java
@@ -4,10 +4,12 @@ package th.in.androidthai.ungfoodshop;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -28,7 +30,54 @@ public class AuthenFragment extends Fragment {
 //        SignUp Controller
         signUpController();
 
+//        SignIn Controller
+        signInController();
+
     }   // Main Method
+
+    private void signInController() {
+        Button button = getView().findViewById(R.id.btnSignIn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText userEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String user = userEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                MyAlert myAlert = new MyAlert(getActivity());
+
+                if (user.isEmpty() || password.isEmpty()) {
+                    myAlert.normalDialog("Have Space", "Please Fill All EveryBlank");
+                } else {
+                    checkAuthen(user, password);
+                }
+
+            }
+        });
+
+    }
+
+    private void checkAuthen(String user, String password) {
+
+        MyConstant myConstant = new MyConstant();
+        MyAlert myAlert = new MyAlert(getActivity());
+
+        try {
+
+            GetUserWhereUserThread getUserWhereUserThread = new GetUserWhereUserThread(getActivity());
+            getUserWhereUserThread.execute(user, myConstant.getUrlGetUserWhereUser());
+
+            String jsonString = getUserWhereUserThread.get();
+            Log.d("8MayV1", "json ==> " + jsonString);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
     private void signUpController() {
         Button button = getView().findViewById(R.id.btnSignUp);
--- app/src/main/java/th/in/androidthai/ungfoodshop/MyConstant.java
+++ app/src/main/java/th/in/androidthai/ungfoodshop/MyConstant.java
@@ -3,6 +3,11 @@ package th.in.androidthai.ungfoodshop;
 public class MyConstant {
 
     private String urlAddUser = "https://www.androidthai.in.th/ruts/addUserUng.php";
+    private String urlGetUserWhereUser = "https://www.androidthai.in.th/ruts/getUserWhereUserUng.php";
+
+    public String getUrlGetUserWhereUser() {
+        return urlGetUserWhereUser;
+    }
 
     public String getUrlAddUser() {
         return urlAddUser;
